d90645e885a106d91cfd5c7f8215feec7c8fe615
tliu <tliu@taosdata.com>
2019-11-05 01:28:34 +0000

    add TDengine serializer

---
+++ bulk_data_gen/common/serializer_tdengine.go
@@ -0,0 +1,317 @@
+package common
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"sync"
+
+	"github.com/pelletier/go-toml"
+)
+
+type serializerTDengine struct {
+}
+
+type TaosPoint struct {
+	Name           string
+	Type           string
+	SourceType     int //1 for tag, 2 for value
+	SourcePosition int //in the origin array
+}
+
+type Schemaconfig struct {
+	Stname    string
+	Suffix    string //suffix is selected from one of the tag/fields value in the input points,stname+suffix make a tablename
+	Suffixpos int
+	Tags      []TaosPoint //tag name and type  pairs set in the config file
+	Fields    []TaosPoint //field name and type  pairs set in the config file
+}
+type TAOSConfig struct {
+	schemaconfigs []Schemaconfig
+}
+
+var dbname string
+var Schematree *toml.Tree
+var IsSTableCreated sync.Map //a map to fast find whether the super table is created
+var IsTableCreated sync.Map  //a map to fast find whether the table is created
+//var Stabletoschema sync.Map      //a map to fast ge the schema
+
+func NewSerializerTDengine(path string, dbn string) *serializerTDengine {
+	var err error
+	Schematree, err = TAOSNewConfig(path)
+	if err != nil {
+		fmt.Println("load taos config failed: %v", err)
+		return nil
+	}
+	dbname = dbn
+
+	return &serializerTDengine{}
+}
+
+// SerializeTDengineBulk writes Point data to the given writer, conforming to the
+// TDengine wire protocol.
+//
+// This function writes output that looks like:
+//Insert into db.tname Values (, , ,)\n
+//
+//
+// TODO(rw): Speed up this function. The bulk of time is spent in strconv.
+func (s *serializerTDengine) SerializePoint(w io.Writer, p *Point) error {
+	err := TAOSCreateStable(w, p)
+	if err != nil {
+		return err
+	}
+
+	str := string(p.MeasurementName)
+	//var schema *Schemaconfig
+
+	out1, ok := IsSTableCreated.Load(str)
+	if ok != true {
+		//fmt.Println("can not load cfg ", str)
+		return nil
+	}
+	//fmt.Println(out1)
+	//fmt.Println(*out1)
+	schema, ok1 := out1.(Schemaconfig)
+	if ok1 != true {
+		info := fmt.Errorf("can not restore from Schemaconfig %s ", schema)
+		return info
+	}
+
+	var tbname string
+	s2 := p.TagValues[schema.Suffixpos]
+	tbname = str + "_" + string(s2[:])
+
+	buf := scratchBufPool.Get().([]byte)
+	//buf = append(buf, "Insert into "...)
+	buf = append(buf, ' ')
+	buf = append(buf, tbname...)
+	buf = append(buf, " using "...)
+	buf = append(buf, str...)
+	buf = append(buf, " tags("...)
+
+	for i := 0; i < len(schema.Tags)-1; i++ {
+		if schema.Tags[i].SourceType == 1 {
+			v := p.TagValues[schema.Tags[i].SourcePosition]
+			buf = fastFormatAppend(v, buf, false)
+			buf = append(buf, ',')
+		} else if schema.Tags[i].SourceType == 2 {
+			v := p.FieldValues[schema.Tags[i].SourcePosition]
+			buf = fastFormatAppend(v, buf, false)
+			buf = append(buf, ',')
+		} else {
+			info := fmt.Sprintf("error type of tSourceType %d,pos %d", schema.Tags[i].SourceType, i)
+			panic(info)
+		}
+	}
+	if schema.Tags[len(schema.Tags)-1].SourceType == 1 {
+		v := p.TagValues[schema.Tags[len(schema.Tags)-1].SourcePosition]
+		buf = fastFormatAppend(v, buf, false)
+		buf = append(buf, ") values("...)
+	} else if schema.Tags[len(schema.Fields)-1].SourceType == 2 {
+		v := p.FieldValues[schema.Tags[len(schema.Tags)-1].SourcePosition]
+		buf = fastFormatAppend(v, buf, false)
+		buf = append(buf, ") values("...)
+	} else {
+		info := fmt.Sprintf("error type of SourceType %d,pos %d", schema.Tags[len(schema.Tags)-1].SourceType, len(schema.Tags)-1)
+		panic(info)
+	}
+
+	buf = fastFormatAppend(p.Timestamp.UTC().UnixNano()/1000000, buf, true)
+	buf = append(buf, ',')
+	for i := 0; i < len(schema.Fields)-1; i++ {
+		if schema.Fields[i].SourceType == 1 {
+			v := p.TagValues[schema.Fields[i].SourcePosition]
+			buf = fastFormatAppend(v, buf, false)
+			buf = append(buf, ',')
+		} else if schema.Fields[i].SourceType == 2 {
+			v := p.FieldValues[schema.Fields[i].SourcePosition]
+			buf = fastFormatAppend(v, buf, false)
+			buf = append(buf, ',')
+		} else {
+			info := fmt.Sprintf("error type of SourceType %d,pos %d", schema.Fields[i].SourceType, i)
+			panic(info)
+		}
+	}
+	if schema.Fields[len(schema.Fields)-1].SourceType == 1 {
+		v := p.TagValues[schema.Fields[len(schema.Fields)-1].SourcePosition]
+		buf = fastFormatAppend(v, buf, false)
+		buf = append(buf, ")"...)
+		buf = append(buf, '\n')
+	} else if schema.Fields[len(schema.Fields)-1].SourceType == 2 {
+		v := p.FieldValues[schema.Fields[len(schema.Fields)-1].SourcePosition]
+		buf = fastFormatAppend(v, buf, false)
+		buf = append(buf, ")"...)
+		buf = append(buf, '\n')
+	} else {
+		info := fmt.Sprintf("error type of SourceType %d,pos %d", schema.Fields[len(schema.Fields)-1].SourceType, len(schema.Fields)-1)
+		panic(info)
+	}
+
+	_, err = w.Write(buf)
+
+	buf = buf[:0]
+	scratchBufPool.Put(buf)
+
+	return err
+}
+
+func (s *serializerTDengine) SerializeSize(w io.Writer, points int64, values int64) error {
+	return serializeSizeInText(w, points, values)
+}
+
+func TAOSNewConfig(path string) (*toml.Tree, error) {
+	var tree *toml.Tree
+	var err error
+
+	tree, err = toml.LoadFile(path)
+
+	if err != nil {
+		return nil, fmt.Errorf("config loading failed: %v", err)
+	}
+	return tree, nil
+}
+
+func LoadSchema(measurement string, tree *toml.Tree) (*TAOSConfig, error) {
+	obj := tree.ToMap()[measurement]
+	//fmt.Println(obj)
+	if obj == nil {
+		return nil, nil
+	}
+	b, err := json.Marshal(obj)
+	if err != nil {
+		return nil, fmt.Errorf("config marshall failed: %v", err)
+	}
+	config := TAOSConfig{}
+	err = json.Unmarshal(b, &config.schemaconfigs)
+	if err != nil {
+		return nil, fmt.Errorf("config unmarshall failed: %v", err)
+	}
+	//var str string = string(b[:])
+
+	return &config, nil
+}
+
+func TAOSCreateStable(w io.Writer, p *Point) error {
+	stablename := string(p.MeasurementName[:])
+	out, _ := IsSTableCreated.Load(stablename)
+
+	if out != nil {
+
+		return nil
+	}
+
+	//IsSTableCreated.Store(stablename,true)
+	s1, err := LoadSchema(stablename, Schematree)
+
+	if err != nil || s1 == nil {
+		return err
+	}
+
+	taosconfig := s1.schemaconfigs[0]
+	//var tbname string
+
+	//var fndflag int = 0
+
+	// find the tag
+	for i := 0; i < len(taosconfig.Tags); i++ {
+		tn := taosconfig.Tags[i].Name
+		found := 0
+		for j := 0; j < len(p.TagKeys); j++ {
+
+			if tn == string(p.TagKeys[j][:]) {
+				taosconfig.Tags[i].SourceType = 1
+				taosconfig.Tags[i].SourcePosition = j
+				found = 1
+				break
+			}
+		}
+		if found == 0 {
+			for j := 0; j < len(p.FieldKeys); j++ {
+
+				if tn == string(p.FieldKeys[j][:]) {
+					taosconfig.Tags[i].SourceType = 2
+					taosconfig.Tags[i].SourcePosition = j
+					found = 1
+					break
+				}
+			}
+		}
+		if found == 0 {
+			info := fmt.Sprintf("Config error, cannot find tagname %s in the point", tn)
+			panic(info)
+		}
+	}
+
+	//find the fields
+	for i := 0; i < len(taosconfig.Fields); i++ {
+		tn := taosconfig.Fields[i].Name
+		found := 0
+		for j := 0; j < len(p.TagKeys); j++ {
+
+			if tn == string(p.TagKeys[j][:]) {
+				taosconfig.Fields[i].SourceType = 1
+				taosconfig.Fields[i].SourcePosition = j
+				found = 1
+				break
+			}
+		}
+		if found == 0 {
+			for j := 0; j < len(p.FieldKeys); j++ {
+
+				if tn == string(p.FieldKeys[j][:]) {
+					taosconfig.Fields[i].SourceType = 2
+					taosconfig.Fields[i].SourcePosition = j
+					found = 1
+					break
+				}
+			}
+		}
+		if found == 0 {
+			info := fmt.Sprintf("Config error, cannot find fieldname %s in the point", tn)
+			panic(info)
+		}
+	}
+
+	var foundtbn int = 0
+	//find the table name suffix
+	tnsuffix := taosconfig.Suffix
+	for i := 0; i < len(p.TagKeys); i++ {
+		tagkey := p.TagKeys[i]
+		tagstr := string(tagkey[:])
+		if tnsuffix == tagstr {
+			taosconfig.Suffixpos = i
+			foundtbn = 1
+			break
+		}
+	}
+
+	if foundtbn == 0 {
+		panic("config error, can not find table suffix ")
+	}
+	//IsTableCreated.Store(tbname,true)
+
+	//store the schema
+	IsSTableCreated.Store(stablename, taosconfig)
+
+	// assemble the create super table command line
+	buf := scratchBufPool.Get().([]byte)
+	s := fmt.Sprintf("create table %s (ts timestamp ", stablename)
+	buf = append(buf, s...)
+	for i := 0; i < len(taosconfig.Fields); i++ {
+		buf = append(buf, ", "+taosconfig.Fields[i].Name+" "+taosconfig.Fields[i].Type...)
+	}
+	buf = append(buf, ") tags ("...)
+	for i := 0; i < len(taosconfig.Tags); i++ {
+		if i == 0 {
+			buf = append(buf, taosconfig.Tags[i].Name+" "+taosconfig.Tags[i].Type...)
+		} else {
+			buf = append(buf, ", "+taosconfig.Tags[i].Name+" "+taosconfig.Tags[i].Type...)
+		}
+	}
+	buf = append(buf, ");\n"...)
+	_, _ = w.Write(buf)
+	buf = buf[:0]
+	scratchBufPool.Put(buf)
+	return nil
+}
---
+++ cmd/bulk_load_tdengine/main.go
@@ -0,0 +1,260 @@
+// bulk_load_tdengine loads a TDengine daemon with data from stdin.
+//
+// The caller is responsible for assuring that the database is empty before
+// bulk load.
+package main
+
+import (
+	"bufio"
+	"database/sql"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/influxdata/influxdb-comparisons/bulk_data_gen/common"
+	_ "github.com/taosSql"
+
+	//	"github.com/caict-benchmark/BDC-TS/util/report"
+	"strconv"
+	"strings"
+	//	"bytes"
+)
+
+// Program option vars:
+var (
+	daemonUrl      string
+	workers        int
+	batchSize      int
+	doLoad         bool
+	reportDatabase string
+	reportHost     string
+	reportUser     string
+	reportPassword string
+	reportTagsCSV  string
+	useCase        string
+)
+
+// Global vars
+var (
+	bufPool    sync.Pool
+	batchChans []chan string
+	//batchChan      chan []string
+	inputDone      chan struct{}
+	workersGroup   sync.WaitGroup
+	reportTags     [][2]string
+	reportHostname string
+	taosDriverName string = "taosSql"
+)
+
+// Parse args:
+func init() {
+	flag.StringVar(&daemonUrl, "url", "127.0.0.1:0", "TDengine URL.")
+
+	flag.IntVar(&batchSize, "batch-size", 100, "Batch size (input items).")
+	flag.IntVar(&workers, "workers", 2, "Number of parallel requests to make.")
+	flag.StringVar(&useCase, "use-case", common.UseCaseChoices[0], "Use case to set specific load behavior. Options: "+strings.Join(common.UseCaseChoices, ","))
+
+	flag.BoolVar(&doLoad, "do-load", true, "Whether to write data. Set this flag to false to check input read speed.")
+
+	flag.StringVar(&reportDatabase, "report-database", "vehicle", "Database name where to store result metrics")
+	flag.StringVar(&reportHost, "report-host", "", "Host to send result metrics")
+	flag.StringVar(&reportUser, "report-user", "", "User for host to send result metrics")
+	flag.StringVar(&reportPassword, "report-password", "", "User password for Host to send result metrics")
+	flag.StringVar(&reportTagsCSV, "report-tags", "", "Comma separated k:v tags to send  alongside result metrics")
+
+	flag.Parse()
+
+	if reportHost != "" {
+		fmt.Printf("results report destination: %v\n", reportHost)
+		fmt.Printf("results report database: %v\n", reportDatabase)
+
+		var err error
+		reportHostname, err = os.Hostname()
+		if err != nil {
+			log.Fatalf("os.Hostname() error: %s", err.Error())
+		}
+		fmt.Printf("hostname for results report: %v\n", reportHostname)
+
+		if reportTagsCSV != "" {
+			pairs := strings.Split(reportTagsCSV, ",")
+			for _, pair := range pairs {
+				fields := strings.SplitN(pair, ":", 2)
+				tagpair := [2]string{fields[0], fields[1]}
+				reportTags = append(reportTags, tagpair)
+			}
+		}
+		fmt.Printf("results report tags: %v\n", reportTags)
+	}
+}
+
+func main() {
+	bufPool = sync.Pool{
+		New: func() interface{} {
+			return make([]string, 0, batchSize)
+		},
+	}
+
+	if doLoad {
+		log.Println("Creating database ----")
+		db, err := sql.Open(taosDriverName, "root:taosdata@/tcp("+daemonUrl+")/vehicle")
+		if err != nil {
+			log.Fatalf("Open database error: %s\n", err)
+		}
+		defer db.Close()
+
+		//fmt.Println(db)
+		createDatabase(db)
+
+		for i := 0; i < workers; i++ {
+			batchChans = append(batchChans, make(chan string, batchSize))
+		}
+		//batchChan = make(chan []string, workers)
+		inputDone = make(chan struct{})
+		log.Println("Starting workers ----")
+		for i := 0; i < workers; i++ {
+			workersGroup.Add(1)
+			go processBatches(i)
+		}
+
+		start := time.Now()
+		itemsRead, bytesRead, valuesRead := scan(db, batchSize)
+		<-inputDone
+		for i := 0; i < workers; i++ {
+			close(batchChans[i])
+		}
+		//close(batchChan)
+		workersGroup.Wait()
+		end := time.Now()
+		took := end.Sub(start)
+
+		itemsRate := float64(itemsRead) / float64(took.Seconds())
+		bytesRate := float64(bytesRead) / float64(took.Seconds())
+		valuesRate := float64(valuesRead) / float64(took.Seconds())
+
+		fmt.Printf("loaded %d items in %fsec with %d workers (mean point rate %.2f/s, mean value rate %.2f/s, %.2fMB/sec from stdin)\n", itemsRead, took.Seconds(), workers, itemsRate, valuesRate, bytesRate/(1<<20))
+	}
+}
+
+func createDatabase(db *sql.DB) {
+	sqlcmd := fmt.Sprintf("Drop database if exists %s", useCase)
+	_, err := db.Exec(sqlcmd)
+	sqlcmd = fmt.Sprintf("create database %s", useCase)
+	_, err = db.Exec(sqlcmd)
+	checkErr(err)
+
+	return
+}
+
+func scan(db *sql.DB, itemsPerBatch int) (int64, int64, int64) {
+	//var n int
+	var vgid int
+	var err error
+	var itemsRead, bytesRead int64
+	var totalPoints, totalValues int64
+	var insertFlag bool = false
+	var cmdinput string
+
+	//buff := bufPool.Get().([]string)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		totalPoints, totalValues, err = common.CheckTotalValues(line)
+		if totalPoints > 0 || totalValues > 0 {
+			continue
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		if strings.HasPrefix(line, "create") {
+			_, err = db.Exec(line)
+		} else {
+			itemsRead++
+			bytesRead += int64(len(scanner.Bytes()))
+			if !doLoad {
+				continue
+			}
+			//buff = append(buff, line)
+			/*
+				n++
+				if n >= itemsPerBatch {
+					batchChan <- buff
+					buff = bufPool.Get().([]string)
+					buff = append(buff, "Insert into")
+					n = 0
+				}
+			*/
+			vgid, _ = strconv.Atoi(line[0:2])
+			vgid = vgid % workers
+			batchChans[vgid] <- line[3:]
+		}
+	}
+	/*
+		if n > 0 {
+			batchChan <- buff
+		}
+	*/
+
+	// Closing inputDone signals to the application that we've read everything and can now shut down.
+	close(inputDone)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Error reading input: %s", err.Error())
+	}
+	if itemsRead != totalPoints {
+		log.Fatalf("Incorrent number of read items: %d, expected: %d:", itemsRead, totalPoints)
+	}
+	return itemsRead, bytesRead, totalValues
+}
+
+func processBatches(iworker int) {
+	var i int
+	db, err := sql.Open(taosDriverName, "root:taosdata@/tcp("+daemonUrl+")/vehicle")
+	checkErr(err)
+	sqlcmd := make([]string, batchSize+1)
+	i = 0
+	sqlcmd[i] = "Insert into"
+	i++
+	/*
+		for batch := range batchChan {
+			if !doLoad {
+				continue
+			}
+			// Write the batch.
+			_, err := db.Exec(strings.Join(batch, ""))
+			if err != nil {
+				log.Fatalf("Error writing: %s\n", err.Error())
+			}
+		}
+	*/
+	for onepoint := range batchChans[iworker] {
+		sqlcmd[i] = onepoint
+		i++
+		if i > batchSize {
+			i = 1
+			_, err := db.Exec(strings.Join(sqlcmd, ""))
+			if err != nil {
+				fmt.Println(sqlcmd)
+				log.Fatalf("Error writing: %s\n", err.Error())
+			}
+		}
+	}
+	if i > 0 {
+		i = 1
+		_, err := db.Exec(strings.Join(sqlcmd, ""))
+		if err != nil {
+			fmt.Println(sqlcmd)
+			log.Fatalf("Error writing: %s\n", err.Error())
+		}
+	}
+
+	workersGroup.Done()
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
