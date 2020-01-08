5e0854dc5e47633a02ac03bde6443ddc673f312b
liu0x54 <tliu@taosdata.com>
2020-01-08 18:33:16 +0800

    modify the run scripts

---
+++ build/tsdbcompare/prepare.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x
+
+#安装docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+
+#安装tdengine客户端
+tar -zxf TDengine-client-1.6.4.5.tar.gz
+cd TDengine-client-1.6.4.5
+./install_client.sh
+cd ..
+
+#拉取influxdb和tdengine的镜像
+docker pull influxdb 
+docker pull tdengine/tdengine 
--- build/tsdbcompare/run.sh
+++ build/tsdbcompare/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 
-
 # Color setting
 RED='\033[0;31m'
 GREEN='\033[1;32m'
@@ -10,21 +9,17 @@ GREEN_UNDERLINE='\033[4;32m'
 NC='\033[0m'
 
 #set -x
-
+echo
 echo "Prepare data for InfluxDB...."
-#bin/bulk_data_gen -seed 123 -format influx-bulk -scale-var 100 -use-case devops -timestamp-start "2018-01-01T00:00:00Z" -timestamp-end "2018-01-02T00:00:00Z" |gzip  >data/influx.dat
+#bin/bulk_data_gen -seed 123 -format influx-bulk -scale-var 100 -use-case devops -timestamp-start "2018-01-01T00:00:00Z" -timestamp-end "2018-01-02T00:00:00Z" >data/influx.dat
 bin/bulk_data_gen -seed 123 -format influx-bulk -sampling-interval 1s -scale-var 10 -use-case devops -timestamp-start "2018-01-01T00:00:00Z" -timestamp-end "2018-01-02T00:00:00Z" >data/influx.dat
 
 echo 
 echo "Prepare data for TDengine...."
+#bin/bulk_data_gen -seed 123 -format tdengine -tdschema-file config/TDengineSchema.toml -scale-var 100 -use-case devops -timestamp-start "2018-01-01T00:00:00Z" -timestamp-end "2018-01-02T00:00:00Z"  > data/tdengine.dat
 bin/bulk_data_gen -seed 123 -format tdengine -sampling-interval 1s -tdschema-file config/TDengineSchema.toml -scale-var 10 -use-case devops -timestamp-start "2018-01-01T00:00:00Z" -timestamp-end "2018-01-02T00:00:00Z"  > data/tdengine.dat
-#bin/bulk_data_gen -seed 123 -format tdengine -tdschema-file config/TDengineSchema.toml -scale-var 100 -use-case devops -timestamp-start "2018-01-01T00:00:00Z" -timestamp-end "2018-01-02T00:00:00Z" |gzip  > data/tdengine.dat
 
-#docker pull influxdb >>/dev/null 2>&1
-#docker pull tdengine/tdengine >>/dev/null 2>&1
 
-#docker run -rm -d influxdb >>/dev/null 2>&1
-#docker run -rm -d tdengine/tdengine >>/dev/null 2>&1
 
 docker network create --ip-range 172.15.1.255/24 --subnet 172.15.1.1/16 tsdbcomp >>/dev/null 2>&1
 
@@ -52,7 +47,7 @@ echo -e "Start test InfluxDB, result in ${GREEN}Green line${NC}"
 
 
 INFLUXRES=`cat data/influx.dat  |bin/bulk_load_influx --batch-size=5000 --workers=20 --urls="http://172.15.1.5:8086" | grep loaded`
-#INFLUXRES=`cat data/influx.dat |gunzip |bin/bulk_load_influx --batch-size=5000 --workers=10 --urls="http://172.15.1.5:8086" | grep loaded`
+
 
 echo
 echo -e "${GREEN}InfluxDB writing result:${NC}"
@@ -66,8 +61,23 @@ echo
 echo  "start query test, query max from 8 hosts group by 1hour, TDengine"
 echo
 
-#bin/bulk_query_gen  -seed 123 -format tdengine -query-type 8-host-allbyhr -scale-var 10 -queries 1000 | bin/query_benchmarker_tdengine -http-client-type cgo -urls="172.15.1.6:0" -workers 50 -print-interval 0
+#Test case 1
+#测试用例1，查询一天的数据中的最大值，用8个hostname标签进行过滤，以1小时为颗粒查询
+#select max(usage_user) from cpu where(hostname='host_a' and hostname='host_b'and hostname='host_c'and hostname='host_d'and hostname='host_e'and hostname='host_f' and hostname='host_g'and hostname='host_h') interval(1h);
+# a,b,c,d,e,f,g,h are random 8 numbers.
 TDQUERY=`bin/bulk_query_gen  -seed 123 -format tdengine -query-type 8-host-allbyhr -scale-var 10 -queries 1000 | bin/query_benchmarker_tdengine  -urls="http://172.15.1.6:6020" -workers 50 -print-interval 0|grep wall`
+
+#Test case 2
+#测试用例2，查询12小时的数据中的最大值，用8个hostname标签进行过滤，以10分钟为颗粒查询
+#select max(usage_user) from cpu where(hostname='host_a' and hostname='host_b'and hostname='host_c'and hostname='host_d'and hostname='host_e'and hostname='host_f' and hostname='host_g'and hostname='host_h') where time >x and time <y interval(10m);
+# a,b,c,d,e,f,g,h are random 8 numbers, y-x =12 hours
+#TDQUERY=`bin/bulk_query_gen  -seed 123 -format tdengine -query-type 8-host-12-hr -scale-var 10 -queries 1000 | bin/query_benchmarker_tdengine  -urls="http://172.15.1.6:6020" -workers 50 -print-interval 0|grep wall`
+
+#Test case 3
+#测试用例3，查询1个小时的数据，用1个hostname进行过滤，以1分钟为颗粒查询
+#select max(usage_user) from cpu where(hostname='host_a') where time >x and time <y interval(10m);
+# a are random number, y-x =1 hours
+#TDQUERY=`bin/bulk_query_gen  -seed 123 -format tdengine -query-type 1-host-1-hr -scale-var 10 -queries 1000 | bin/query_benchmarker_tdengine  -urls="http://172.15.1.6:6020" -workers 50 -print-interval 0|grep wall`
 
 echo
 echo -e "${GREEN}TDengine query result:${NC}"
@@ -80,7 +90,23 @@ sleep 10
 echo 
 echo  "start query test, query max from 8 hosts group by 1hour, Influxdb"
 echo
+#Test case 1
+#测试用例1，查询一天的数据中的最大值，用8个hostname标签进行过滤，以1小时为颗粒查询
+#select max(usage_user) from cpu where(hostname='host_a' and hostname='host_b'and hostname='host_c'and hostname='host_d'and hostname='host_e'and hostname='host_f' and hostname='host_g'and hostname='host_h') interval(1h);
+# a,b,c,d,e,f,g,h are random 8 numbers.
 INFLUXQUERY=`bin/bulk_query_gen  -seed 123 -format influx-http -query-type 8-host-allbyhr -scale-var 10 -queries 1000 | bin/query_benchmarker_influxdb  -urls="http://172.15.1.5:8086"  -workers 50 -print-interval 0|grep wall`
+
+#Test case 2
+#测试用例2，查询12小时的数据中的最大值，用8个hostname标签进行过滤，以10分钟为颗粒查询
+#select max(usage_user) from cpu where(hostname='host_a' and hostname='host_b'and hostname='host_c'and hostname='host_d'and hostname='host_e'and hostname='host_f' and hostname='host_g'and hostname='host_h') where time >x and time <y interval(10m);
+# a,b,c,d,e,f,g,h are random 8 numbers, y-x =12 hours
+#INFLUXQUERY=`bin/bulk_query_gen  -seed 123 -format influx-http -query-type 8-host-12-hr -scale-var 10 -queries 1000 | bin/query_benchmarker_influxdb  -urls="http://172.15.1.5:8086"  -workers 50 -print-interval 0|grep wall`
+
+#Test case 3
+#测试用例3，查询1个小时的数据，用1个hostname进行过滤，以1分钟为颗粒查询
+#select max(usage_user) from cpu where(hostname='host_a') where time >x and time <y interval(10m);
+# a are random number, y-x =1 hours
+#INFLUXQUERY=`bin/bulk_query_gen  -seed 123 -format influx-http -query-type 1-host-1-hr -scale-var 10 -queries 1000 | bin/query_benchmarker_influxdb  -urls="http://172.15.1.5:8086"  -workers 50 -print-interval 0|grep wall`
 
 echo
 echo -e "${GREEN}InfluxDB query result:${NC}"
@@ -108,7 +134,6 @@ docker stop $INFLUX >>/dev/null 2>&1
 docker container rm -f $INFLUX >>/dev/null 2>&1
 docker stop $TDENGINE >>/dev/null 2>&1
 docker container rm -f $TDENGINE >>/dev/null 2>&1
-#docker network rm tsdbcomp >>/dev/null 2>&1
+docker network rm tsdbcomp >>/dev/null 2>&1
 #bulk_query_gen/bulk_query_gen  -format influx-http -query-type 1-host-1-hr -scale-var 10 -queries 1000 | query_benchmarker_influxdb/query_benchmarker_influxdb  -urls="http://172.26.89.231:8086" 
 #bulk_query_gen/bulk_query_gen  -format tdengine -query-type 1-host-1-hr -scale-var 10 -queries 1000 | query_benchmarker_tdengine/query_benchmarker_tdengine  -urls="http://172.26.89.231:6020" 
-
