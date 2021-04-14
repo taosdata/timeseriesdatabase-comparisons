f1aa8c1d362ea8cf06fc49f1b6b685ee2d85c8c6
liuyq-617 <yqliu@taosdata.com>
2021-04-14 14:35:35 +0800

    client and server deployed in diffrent servers

---
+++ build/tsdbcompare/loop_scale_to_server.sh
@@ -0,0 +1,8 @@
+for s in {100,200,400,600,800,1000};do
+    for i in {5000,2500,1000,1};do
+		./write_to_server.sh -b $i -w 100 -g 1 -s $s
+        for j in {50,16,1};do
+            ./write_to_server.sh -b $i -w $j -g 0 -s $s
+        done
+    done
+done
