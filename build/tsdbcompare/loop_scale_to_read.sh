36045077a72abcdc01f4e098e681633ece4df606
bryanchang0603 <changb9@mcamster.ca>
2021-07-06 16:31:17 +0800

    try to implement all in one

--- build/tsdbcompare/loop_scale_to_read.sh
+++ build/tsdbcompare/loop_scale_to_read.sh
@@ -1,3 +1,4 @@
+echo "run query test"
 for s in {100, 1000};do
 	./read.sh -w 100 -g 1 -s $s
     for j in {50,16,1};do
--- build/tsdbcompare/loop_scale_to_server.sh
+++ build/tsdbcompare/loop_scale_to_server.sh
@@ -1,3 +1,4 @@
+echo "run insert test"
 for s in {100,200,400,600,800,1000};do
     for i in {2000,1000,500,1};do
 		./write_to_server.sh -b $i -w 100 -g 1 -s $s
