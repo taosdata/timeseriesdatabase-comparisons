20b6df2671380723ded34552e086f4207c5ceb25
liuyq-617 <yqliu@taosdata.com>
2021-07-13 02:06:02 +0000

    update test case

--- build/tsdbcompare/loop_scale_to_read.sh
+++ build/tsdbcompare/loop_scale_to_read.sh
@@ -1,7 +1,14 @@
 echo "run query test"
-for s in {100, 1000};do
-	./read.sh -w 100 -g 0 -s $s
+for s in {100,1000};do
+	./read.sh -w 100 -g 1 -s $s
     for j in {50,16,1};do
-        ./write_to_server.sh -w $j -g 0 -s $s
+        ./read.sh -w $j -g 0 -s $s
     done
 done
+for s in {100,1000};do
+	./read.sh -w 100 -g 1 -s $s -n 'fast'
+    for j in {50,16,1};do
+        ./read.sh -w $j -g 0 -s $s -n 'fast'
+    done
+done
+
