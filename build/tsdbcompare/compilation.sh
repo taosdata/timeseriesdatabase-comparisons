c3b84a13cff35d3e20e5e9b779b7d1712ea5a988
chenhaoran <haoran920c@163.com>
2022-02-26 12:19:20 +0800

    modify comment in compilation.sh

--- build/tsdbcompare/compilation.sh
+++ build/tsdbcompare/compilation.sh
@@ -1,7 +1,7 @@
 # new work dir
 mkdir /tscompar-tests
 cd /tscompar-tests
-# clone project and prepare to compile and compile programe
+# clone project and prepare to compile and compile program
 git clone https://github.com/taosdata/timeseriesdatabase-comparisons
 cd timeseriesdatabase-comparisons && rm -rf go.mod go.sum 
 go mod init github.com/taosdata/timeseriesdatabase-comparisons
@@ -11,6 +11,7 @@ go get github.com/pelletier/go-toml
 go get github.com/pkg/profile
 go get github.com/valyala/fasthttp
 
+# generate program
 mkdir -p build/tsdbcompare/bin
 cd cmd/bulk_data_gen ;go build ;cp bulk_data_gen ../../build/tsdbcompare/bin
 cd ../bulk_load_influx;go build ;cp bulk_load_influx ../../build/tsdbcompare/bin
